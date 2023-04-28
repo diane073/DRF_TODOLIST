5566575c5f1628f382ef9a6ba2879cab050c19b4
dana <bear9473@gmail.com>
2023-04-28 18:46:59 +0900

    bugfix: todolist update

--- todolists/views.py
+++ todolists/views.py
@@ -1,14 +1,15 @@
 from rest_framework.views import APIView
-from rest_framework import status
+from rest_framework import status, permissions
 from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 from todolists.serializers import ToDoListSerializer, ToDoListCreateSerializer
 from todolists.models import ToDoList
-
 # Create your views here.
 
 
 class ToDoListView(APIView):
+    permission_class = [permissions.IsAuthenticated]
+
     def get(self, request):
         todolist = ToDoList.objects.filter(user=request.user).order_by('-created_date')
         serializer = ToDoListSerializer(todolist, many=True)
@@ -24,7 +25,7 @@ class ToDoListView(APIView):
 
 
 class ToDoListDetailView(APIView):
-    def post(self, request, todolist_id):
+    def post(self, request, todolist_id): #할일 완료
         todo = get_object_or_404(ToDoList, id=todolist_id)
         if todo.is_completed == True:
             todo.update(completion_at = todo.completion_at)
@@ -32,12 +33,13 @@ class ToDoListDetailView(APIView):
         else:
             return Response(status=status.HTTP_400_BAD_REQUEST)
 
-    def put(self, request, todolist_id):
+    def put(self, request, todolist_id): #내용수정
         todo = get_object_or_404(ToDoList, id=todolist_id)
         if request.user == todo.user:
-            serializer = ToDoListCreateSerializer(data=request.data)
+            serializer = ToDoListCreateSerializer(todo, data=request.data) #여기에 ㅅ
+
             if serializer.is_valid():
-                serializer.save(user=request.user)
+                serializer.save()
                 return Response({'message':f'수정 완료! ${serializer.data}'}, status=status.HTTP_201_CREATED)
             else:
                 return Response({'message':f'${serializer.errors}'}, status=status.HTTP_400_BAD_REQUEST)
